08dbf9ea3ee877e8d390ee847282269f85d1e0d3
Ewen <ewen2252@gmail.com>
2024-10-01 12:57:19 +0200

    Exercice 4

---
+++ code/Exercise4/classExemple/Group.java
@@ -0,0 +1,25 @@
+public class Group {
+
+    private int weight;
+    private String name;
+    private Color color;
+
+    public Group(String name, Color color, int weight) {
+        this.name = name;
+        this.color = color;
+        this.weight = weight;
+    }
+
+    public int compareTo(Group other) {
+        return weight - other.weight;
+    }
+
+    public void draw() {
+        Screen.rectangle(color, name);
+    }
+
+    public String getName() { 
+        return this.name; 
+    }
+
+}
